6f1094a03fc05ac685e1cef32be9fa8b25fccd77
Marco Bakera <marco@bakera.de>
2020-06-14 09:57:34 +0200

    init

---
+++ download.py
@@ -0,0 +1,31 @@
+import urllib.request
+import datetime
+
+BASE = "http://archive.luftdaten.info"
+
+def download(url):
+    'Download and return data from the given URL.'
+    data = urllib.request.urlopen(url).read()
+    return data
+
+def save(data, filename):
+    'Save data into the given filename.'
+    with open(filename, 'wt') as f:
+        f.write(data)
+
+def download_days(number_of_days):
+    'Download and save data for a given number of days.'
+    one_day = datetime.timedelta(days=1)
+
+    for i in range(1, number_of_days + 1): # no data for today
+        current_date = datetime.date.today() - i * one_day
+        base_url = f'{BASE}/{current_date}/{current_date}'
+
+        for sensor in ['sds011_sensor_3659', 'dht22_sensor_3660']:
+            url = f'{base_url}_{sensor}.csv'
+            print('download', url)
+            data = str(download(url), encoding='UTF-8')
+            save(data, f'{current_date}_{sensor}.csv')
+
+if __name__ == '__main__':
+    download_days(30)
