488be3403f9d9d3de9d9a11d2b4e31e28fd9c6ab
Marco Bakera <marco@bakera.de>
2020-06-17 15:31:07 +0200

    don't check certs.

--- download_archive.sh
+++ download_archive.sh
@@ -10,8 +10,8 @@ for y in 2019 2018 2017; do
     for m in $(seq -w 12); do
         for d in $(seq -w 31); do
             # -b: run in background
-            wget -b "$BASE/$y-$m-$d/$y-$m-${d}_sds011_sensor_3659.csv";
-            wget -b "$BASE/$y-$m-$d/$y-$m-${d}_dht22_sensor_3660.csv";
+            wget -b --no-check-certificate "$BASE/$y-$m-$d/$y-$m-${d}_sds011_sensor_3659.csv";
+            wget -b --no-check-certificate "$BASE/$y-$m-$d/$y-$m-${d}_dht22_sensor_3660.csv";
         done
     done
     sleep 4;  # wait some seconds for processes to finish
