cdb4a30c707004a2f985ee7b88fb63709992f414
Marco Bakera <marco@bakera.de>
2021-03-13 12:43:09 +0100

    refactor, add PKs

--- import_from_archive_into_db.sh
+++ import_from_archive_into_db.sh
@@ -1,37 +1,62 @@
 #!/bin/bash
 
-SQLCREATETABLE1="CREATE TABLE temp_hum (sensor_id int, sensor_type text, location int, lat real, lon real, timestamp datetime, temperature real, humidity real);"
+SQLCREATETABLE1="CREATE TABLE temp_hum 
+(sensor_id int, 
+ sensor_type text, 
+ location int, 
+ lat real, 
+ lon real, 
+ timestamp datetime, 
+ temperature real, 
+ humidity real,
+ PRIMARY KEY (timestamp))"
 
-SQLCREATETABLE2="CREATE TABLE feinstaub (sensor_id int, sensor_type text, location int, lat real, lon real, timestamp datetime, P1 real, durP1 real, ratioP1 real,P2 real,durP2 real, ratioP2 real);"
+SQLCREATETABLE2="CREATE TABLE feinstaub 
+(sensor_id int, 
+ sensor_type text, 
+ location int, 
+ lat real, 
+ lon real, 
+ timestamp datetime, 
+ P1 real, 
+ durP1 real, 
+ ratioP1 real,
+ P2 real,
+ durP2 real, 
+ ratioP2 real,
+ PRIMARY KEY (timestamp))"
 
 SQL_TIME_SPLIT='CAST(strftime("%Y",timestamp) AS INTEGER) year, CAST(strftime("%m",timestamp) AS INTEGER) month, CAST(strftime("%d",timestamp) AS INTEGER) day, CAST(strftime("%H",timestamp) AS INTEGER) hour, CAST(strftime("%M",timestamp) AS INTEGER) minute'
 
 DB=sensordata.db
 
-sqlite3 $DB "$SQLCREATETABLE1"
-sqlite3 $DB "$SQLCREATETABLE2"
+sqlite3 $DB "$SQLCREATETABLE1" || exit
+sqlite3 $DB "$SQLCREATETABLE2" || exit
 
+echo -en "importing sensor 3660. \nFiles to be processed: "
+ls archive/*3660.csv | wc -l
 for f in archive/*3660.csv; do
-    echo "importing $f"
-    sqlite3 $DB -csv -csv -separator \; ".import $f temp_hum"
+    sqlite3 $DB --csv --separator \; ".import --skip 1 $f temp_hum" || exit
 done
-# import contains first line of csv-file. get rid of if
-sqlite3 $DB 'DELETE FROM temp_hum WHERE sensor_id<>3660'
+echo "Some entries"
+sqlite3 $DB -box 'SELECT * FROM temp_hum limit 10'
+sqlite3 $DB -box 'SELECT count(*) NumEntries FROM temp_hum'
 
-sqlite3 $DB -cmd '.headers on' -cmd '.mode column' 'SELECT * FROM temp_hum limit 10;'
 
-
+echo -en "importing sensor 3659.\nFiles to be processed: "
+ls archive/*3659.csv | wc -l
 for f in archive/*3659.csv; do
-    echo "importing $f"
-    sqlite3 $DB -csv -csv -separator \; ".import $f feinstaub"
+    sqlite3 $DB --csv --separator \; ".import --skip 1 $f feinstaub" || exit
 done
-# import contains first line of csv-file. get rid of if
-sqlite3 $DB 'DELETE FROM feinstaub WHERE sensor_id<>3659'
 
-sqlite3 $DB -cmd '.headers on' -cmd '.mode column' 'SELECT * FROM feinstaub limit 10;'
+echo "some entries"
+sqlite3 $DB -box 'SELECT * FROM feinstaub limit 10'
+sqlite3 $DB -box 'SELECT count(*) NumEntries FROM feinstaub'
 
-# creating views 
+echo "creating views" 
+sqlite3 $DB "CREATE VIEW temp_hum_view AS 
+ SELECT ${SQL_TIME_SPLIT}, temperature, humidity from temp_hum"
 
-sqlite3 $DB "CREATE VIEW temp_hum_view AS SELECT ${SQL_TIME_SPLIT}, temperature, humidity from temp_hum"
+sqlite3 $DB "CREATE VIEW feinstaub_view AS 
+ SELECT ${SQL_TIME_SPLIT}, p1, p2 from feinstaub"
 
-sqlite3 $DB "CREATE VIEW feinstaub_view AS SELECT ${SQL_TIME_SPLIT}, p1, p2 from feinstaub"
